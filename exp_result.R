67f7087ea7bc8c3913499d9447bfa69293b91e03
Ansel <gwan0@jupyter-hub.asia-northeast3-a.c.m5-forecast.internal>
2020-03-18 14:25:08 +0000

    cc

---
+++ exp_result.R
@@ -0,0 +1,10 @@
+##kernel full_data, no cv
+##Best score: 2.262958 at 1941 iteration
+## valid's rmse:2.0569
+
+##kernel full_data, no cv
+##Best score: 2.262958 at 1941 iteration
+##
+
+## 
+##[1301]:	train's rmse:2.08749	valid's rmse:2.05684 
